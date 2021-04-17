cac175eac891e2cf197dde67de679d39fd24872f
Devan Wong <devan.wong@yahoo.com>
2021-04-17 08:34:19 -0700

    add findall APIs to the sq details and sq list page

--- src/app/security-question-details/security-question-details.component.ts
+++ src/app/security-question-details/security-question-details.component.ts
@@ -7,9 +7,14 @@
 ; Description: typescript component file focusing on the security questions details page
 ;===========================================
 */
-
-
+// This was auto generated
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { SecurityQuestionService } from '../security-question.service';
+import { SecurityQuestion } from '../security-question.interface';
 
 @Component({
   selector: 'app-security-question-details',
@@ -18,9 +23,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityQuestionDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private fb: FormBuilder, private router: Router, private securityQuestionService: SecurityQuestionService) {
+    this.questionId = this.route.snapshot.paramMap.get('questionId');
 
-  ngOnInit(): void {
+    // FindSecurityQuestionById: Erica
+
+
   }
 
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      text: [null, Validators.compose([Validators.required])]
+    })
+  }
+
+  // Save question function: Devan
+  saveQuestion() {
+    const updatedSecurityQuestion = {} as SecurityQuestion;
+    updatedSecurityQuestion.text = this.form.controls.text.value;
+
+    this.securityQuestionService.updateSecurityQuestion(this.questionId, updatedSecurityQuestion).subscribe(res => {
+      this.router.navigate(['/security-questions'])
+    })
+  }
+  // Cancel Button
+  cancel() {
+    this.router.navigate(['/secuirty-questions']);
+  }
 }
--- src/app/security-question-list/security-question-list.component.ts
+++ src/app/security-question-list/security-question-list.component.ts
@@ -7,19 +7,36 @@
 ; Description: typescript component file focusing on the security question list page
 ;===========================================
 */
-
+// This was auto generated
 import { Component, OnInit } from '@angular/core';
+// Imported the values for the security question
+import { SecurityQuestion } from '../security-question.interface';
+import { HttpClient } from '@angular/common/http';
+import { SecurityQuestionService } from '../security-question.service';
 
 @Component({
   selector: 'app-security-question-list',
   templateUrl: './security-question-list.component.html',
   styleUrls: ['./security-question-list.component.css']
 })
-export class SecurityQuestionListComponent implements OnInit {
 
-  constructor() { }
+export class SecurityQuestionListComponent implements OnInit {
+  securityQuestions: SecurityQuestion[];
+  displayedColumns = ['question', 'functions'];
+
+  constructor(private http: HttpClient, private dialog: MatDialog, private securityQuestionService: SecurityQuestionService) {
+    // Subscribing to the findAllSecurityQuestion
+    this.securityQuestionService.findAllSecurityQuestions().subscribe(res => {
+      this.securityQuestions = res['data'];
+    },  err => {
+      console.log(err);
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  // DeleteSecurity API: Erica
+
+  // Mat Dialog function for pop up feature: Erica
 }
