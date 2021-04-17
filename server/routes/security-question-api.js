7cbf115c887098a4c8ba2f44b80320027fda7135
Devan Wong <devan.wong@yahoo.com>
2021-04-17 09:56:25 -0700

    added security questions findall and update api that was assigned to me (devan)

--- server/routes/security-question-api.js
+++ server/routes/security-question-api.js
@@ -14,22 +14,89 @@ const BaseResponse = require('../services/base-response');
 const router = express.Router();
 
 /*
-FindAll - Erica
+FindAll - Devan
 */
-
+router.get('/', async(req,res) => {
+  try
+  {
+    SecurityQuestion.find({})
+    .where('isDisabled')
+    .equals(false)
+    .exec(function(err, securityQuestions)
+    {
+      if(err)
+      {
+        console.log(err);
+        const findAllMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(findAllMongodbErrorResponse.toObject());
+      }
+      else
+      {
+        console.log(securityQuestions);
+        const findAllResponse = new BaseResponse(200, 'Query successful',securityQuestions)
+        res.json(findAllResponse.toObject());
+      }
+    })
+  }
+  catch (e)
+  {
+    console.log(e);
+    const findAllCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
+    res.status(500).send(findAllCatchErrorResponse.toObject());
+  }
+});
 /*
 FindById - Erica
 */
 
 /*
-CreateSecurityApi - Erica
+CreateSecurityApi - Anil
 */
 
 /*
 UpdateSecurityAPI - Devan
 */
+router.put('/:id', async(req, res) => {
+  try
+  {
+    SecurityQuestion.findOne({'_id': req.params.id}, function(err, securityQuestion){
+      if(err)
+      {
+        console.log(err);
+        const updateSecurityQuestionMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(updateSecurityQuestionMongodbErrorResponse.toObject());
+      }
+      else
+      {
+        console.log(securityQuestion);
+        securityQuestion.set({
+          text: req.body.text
+        });
 
-
+        securityQuestion.save(function(err, savedSecurityQuestion) {
+          if (err)
+          {
+            console.log(err);
+            const savedSecurityQuestionMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+            res.status(500).send(savedSecurityQuestionMongodbErrorResponse.toObject());
+          }
+          else
+          {
+            console.log(savedSecurityQuestion);
+            const updateSecurityQuestionResponse = new BaseResponse(200, 'Query sucessful', savedSecurityQuestion);
+            res.json(updateSecurityQuestionResponse.toObject());
+          }
+        })
+      }
+    })
+  }
+  catch (e)
+  {
+    console.log(e);
+    const updateSecurityQuestionCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
+    res.status(500).send(updateSecurityQuestionCatchErrorResponse.toObject());
+  }
+});
 
 /*
 DeleteSecurityAPI - Erica
