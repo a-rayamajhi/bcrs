53bb1137bdb1af542cd55a42000505ba816ce871
Devan Wong <devan.wong@yahoo.com>
2021-04-17 09:05:00 -0700

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,27 @@
 ;===========================================
 */
 
+// Import Components
 import { HomeComponent } from './pages/home/home.component';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { UserCreateComponent } from './pages/user-create/user-create.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { UserDetailsComponent } from './pages/user-details/user-details.component';
+import { SecurityQuestionListComponent } from './pages/security-question-list/security-question-list.component';
+import { SecurityQuestionDetailsComponent } from './pages/security-question-details/security-question-details.component';
+import { SecurityQuestionCreateComponent } from './pages/security-question-create/security-question-create.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+
+// Import angulars
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// Import Guards
+import { AuthGuard } from './shared/auth.guard';
+import { SecurityQuestionService } from './shared/security-question.service';
+
+
+
 
 const routes: Routes = [
   {
@@ -21,9 +38,44 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent
+      },
+      {
+        path: 'users',
+        component: UserListComponent
+      },
+      {
+        path: 'users/:usersId',
+        component: UserDetailsComponent
+      },
+      {
+        path: 'users/create/new',
+        component: UserCreateComponent
+      },
+      {
+        path: 'security-questions',
+        component: SecurityQuestionListComponent
+      },
+      {
+        path: 'security-questions/:questionId',
+        component: SecurityQuestionDetailsComponent
+      },
+      {
+        path: 'security-questions/create/new',
+        component: SecurityQuestionCreateComponent
       }
-    ]
-  }
+    ],
+    canActivate: [AuthGuard]
+  },
+    {
+      path: 'session',
+      component: AuthLayoutComponent,
+      children: [
+        {
+          path: 'signin',
+          component: SigninComponent
+        }
+      ]
+    }
 ];
 
 @NgModule({
