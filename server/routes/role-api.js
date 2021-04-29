2913848bd9fddcd194ecfd57eec3e4cc1a696064
Devan Wong <devan.wong@yahoo.com>
2021-04-29 08:47:19 -0700

    added findAllRole and updateRole  to the Role-api

--- server/routes/role-api.js
+++ server/routes/role-api.js
@@ -8,12 +8,62 @@
 ;===========================================
 */
 // Require Statements
+const Role = require('../models/role');
+
+
+const router = express.Router();
 
 
 /**
- * Find All - Devan
+ * FindAll API
+ * Method: GET
+ *
+ * @return JSON object with array of all roles with isDisabled set to false
  */
-
+ router.get('/', async (req, res) => {
+   try
+   {
+     Role.find({})
+      .where('isDisabled')
+      .equals(false)
+      .exec(function(err, roles)
+      // handle mongoDB error
+      {
+        if (err)
+        {
+          console.log(err);
+          const findAllRolesMongodbErrorResponse = new ErrorResponse(
+            '500',
+            'Internal server error',
+            err
+          );
+          res.status(500).send(findAllRolesMongodbErrorResponse.toObject());
+        }
+        else
+        // return roles with isDisabled set to false
+        {
+          console.log(roles);
+          const findAllRolesResponse = new BaseResponse(
+            '200',
+            'Query Successful',
+            roles
+          );
+          res.json(findAllRolesResponse.toObject());
+        }
+      })
+    }
+   catch (e)
+   // Server error
+   {
+     console.log(e);
+     const findAllRolesCatchErrorResponse = new ErrorResponse(
+       '500',
+       'Internal Server Error',
+       e.message
+      );
+     res.status(500).send(findAllRolesCatchErrorResponse.toObject());
+   }
+ });
 
  /**
  * Find By Id - Anil
@@ -25,11 +75,75 @@
  */
 
 
-  /**
- * Update Role - Devan
+/**
+ * UpdateRoleAPI
+ * Method: PUT
+ *
+ * @return Update Role
  */
+router.put('/:roleId', async(req, res) => {
+  try
+  {
+    Role.findOne(
+      {'_id': req.params.roleId},
+      function(err, role)
+      // handle mongoDB error
+    {
+      if (err)
+      {
+        console.log(err);
+        const updateRoleMongodbErrorResponse = new ErrorResponse(
+          '500',
+          'Internal server error',
+          err
+        );
+        res.status(500).send(updateRoleMongodbErrorResponse.toObject());
+      }
+      else
+      {
+        console.log(role);
+        // Scaffold Role with request data
+        role.set({
+          text: req.body.text
+        });
+        // Persist new Role
+        role.save(function(err, updatedRole)
+        // handle mongoDB error
+        {
+          if (err)
+          {
+            console.log(err);
+            const updatedRoleMongodbErrorResponse = new ErrorResponse(
+              '500',
+              'Internal server error',
+              err
+            );
+            res.status(500).send(updatedRoleMongodbErrorResponse.toObject());
+          }
+          else
+          // return Saved Security Question
+          {
+            console.log(updatedRole);
+            const updateRoleResponse = new BaseResponse(
+              '200',
+              'Query Successful',
+              updatedRole
+            );
+            res.json(updateRoleResponse.toObject());
+          }
+        })
+      }
+    })
+  }
+  catch (e)
+  {
+    // Server error
+    console.log(e);
+    const updatedRoleCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+    res.status(500).send(updatedRoleCatchErrorResponse.toObject());
+  }
+})
 
-
-  /**
+/**
  * Delete Role - Anil
  */
