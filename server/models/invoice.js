dee33493b55c167a1e5b3b5105afd20a59484b71
Devan Wong <devan.wong@yahoo.com>
2021-04-29 08:06:18 -0700

    role.js modal is complete

--- server/models/invoice.js
+++ server/models/invoice.js
@@ -2,10 +2,10 @@
 ============================================
 ; Title: invoice modal
 ; Author: Professor Krasso
-; Date:   28 Apr 2021
-; Modified by:
+; Date:   29 Apr 2021
+; Modified by: Devan Wong
 ; Description: connecting to mongodb
 ;===========================================
 */
 
-// ANIL
+// Devan
--- server/models/roles.js
+++ server/models/roles.js
@@ -1,10 +1,25 @@
 /*
 ============================================
-; Title: Roles modal
+; Title: Roles models
 ; Author: Professor Krasso
-; Date:   28 Apr 2021
-; Modified by:
+; Date:   29 Apr 2021
+; Modified by: Devan Wong
 ; Description: connecting to mongodb
 ;===========================================
 */
-// Devan
+// Import mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let roleSchema = new Schema ({
+  text: {
+    type: String,
+    unique: true
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('Role', Schema);
