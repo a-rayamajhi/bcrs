9bdb34ba2226065261620258397a91a5c342e0fe
Devan Wong <devan.wong@yahoo.com>
2021-04-29 11:13:47 -0700

    edits to the role-details component page

--- src/app/pages/role-details/role-details.component.ts
+++ src/app/pages/role-details/role-details.component.ts
@@ -2,13 +2,17 @@
 ============================================
 ; Title:  Role Details
 ; Author: Professor Krasso
-; Date:   28 Apr 2021
-; Modified by:
+; Date:   29 Apr 2021
+; Modified by: Devan Wong
 ; Description: role-details component page
 ;===========================================
 */
 
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RoleService } from 'src/app/shared/services/role.service';
+import { IRole } from 'src/app/shared/interfaces/role.interface';
 
 @Component({
   selector: 'app-role-details',
@@ -16,20 +20,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role-details.component.css']
 })
 export class RoleDetailsComponent implements OnInit {
+  form: FormGroup;
+  role: IRole;
+  roleId: string;
 
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router,
+    private roleService: RoleService
+  ) {
   /**
-   * findRoleNById - Anil
+   * findRoleById - Anil
    */
-  constructor() { }
+
+   }
 
   ngOnInit(): void {
+    // Form builder
   }
 
   /**
-   * Save function - DEVAN
+   * Save function
    */
+  save() {
+    const updatedRole = {
+      text: this.form.controls['text'].value
+    } as IRole;
+
+    this.roleService.updateRole(this.roleId, updatedRole).subscribe(res => {
+      this.router.navigate(['/roles']);
+    }, err => {
+      console.log(err);
+    })
+  }
 
   /**
    * Cancel function - DEVAN
    */
+  cancel() {
+    this.router.navigate(['/roles']);
+  }
 }
