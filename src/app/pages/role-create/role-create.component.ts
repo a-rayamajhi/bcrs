e0a7e5fe914ed8e95e6edfa5661f42c2732f83cc
Devan Wong <devan.wong@yahoo.com>
2021-04-29 11:07:22 -0700

    role-create html page

--- src/app/pages/role-create/role-create.component.ts
+++ src/app/pages/role-create/role-create.component.ts
@@ -2,14 +2,17 @@
 ============================================
 ; Title:  Role Create
 ; Author: Professor Krasso
-; Date:   28 Apr 2021
+; Date:   29 Apr 2021
 ; Modified by:
 ; Description: role-create component page
 ;===========================================
 */
 
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RoleService } from 'src/app/shared/services/role.service';
+import { IRole } from '../../shared/interfaces/role.interface';
 @Component({
   selector: 'app-role-create',
   templateUrl: './role-create.component.html',
@@ -17,7 +20,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoleCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private roleService: RoleService
+    ) { }
 
   ngOnInit(): void {
   }
