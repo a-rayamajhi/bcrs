ec2f3d86f127bba58964b6c530acb60843e94e8e
Devan Wong <devan.wong@yahoo.com>
2021-04-29 11:17:43 -0700

    edits to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,21 @@
 ============================================
 ; Title:  Home Component TS.
 ; Author: Professor Krasso
-; Date:   28 Apr 2021
+; Date:   29 Apr 2021
 ; Modified by: Devan Wong
 ; Description: Home component.ts page
 ;===========================================
 */
 
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { IServiceRepairItem } from 'src/app/shared/interfaces/service-repair-item.interface';
+import { ILineItem } from 'src/app/shared/interfaces/line-item.interface';
+import { CookieService } from 'ngx-cookie-service';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ServiceRepairService } from 'src/app/shared/services/service-repair.service';
+import { InvoiceService } from 'src/app/shared/services/invoice.service';
 
 @Component({
   selector: 'app-home',
@@ -16,14 +24,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  form: FormGroup;
+  userName: string;
+  services: IServiceRepairItem[];
+  lineItems: ILineItem[];
 
-  constructor() { }
+  constructor(
+    private cookieService: CookieService,
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+    private router: Router,
+    private serviceRepairService: ServiceRepairService,
+    private invoiceService: InvoiceService
+  ) {
+    // get a username
+   }
 
-  // get the username
+
   ngOnInit(): void {
+    // Form Builder
   }
 
   /**
-   * Submit form
+   * Submit form - ANIL.
    */
 }
